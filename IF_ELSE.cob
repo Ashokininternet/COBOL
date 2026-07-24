5e2a68089e09c07c34cd08b0bd24b226bb369166
Ashok S.R <ashok.106sr@gmail.com>
2026-07-24 12:43:23 +0530

    learn if and else in cobol and figuring out range and thru

---
+++ IF_ELSE.cob
@@ -0,0 +1,21 @@
+       >>SOURCE FORMAT FREE
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. IF_ELSE.
+       AUTHOR. ASHOK.
+       DATE-WRITTEN. Jul 24 2026.
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+
+       01 AGE PIC 99 VALUE ZERO.
+       PROCEDURE DIVISION.
+           DISPLAY "Enter your age: " WITH NO ADVANCING
+           ACCEPT AGE
+           IF AGE > 18 THEN
+             DISPLAY "You can vote"
+           ELSE
+             DISPLAY "You cant vote"
+           END-IF
+           STOP RUN.
--- Range.cob
+++ Range.cob
@@ -6,20 +6,23 @@
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SPECIAL-NAMES.
-           CLASS PassingScore IS "A" THRU "C, D".
+           CLASS PassingScore IS "A" THRU "C".
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
        01 AGE PIC 99 VALUE ZERO.
        01 Grade PIC 99 VALUE ZERO.
        01 Score PIC X(1) VALUE "B".
+       01 VAL PIC 99 VALUE ZERO.
        01 CanVoteFlag PIC 9 VALUE 0.
            88 CanVote VALUE 1.
            88 CantVote VALUE 0.
        01 TestNum PIC X.
            88 IsPrime VALUE "1", "3", "5", "7".
-           88 IsOdd VALUE "1", "3", "5", "7".
+           88 IsOdd VALUE "1", "3", "5", "7", "9".
+           88 IsEven VALUE "2", "4", "6", "8".
+           88 LessThan5 VALUE "1", THRU "4".
+           88 ANumber VALUE "0" THRU "9".
 
        PROCEDURE DIVISION.
-           DISPLAY "Hello"
            STOP RUN.
